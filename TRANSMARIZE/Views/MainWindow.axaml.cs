cf3012961aaa5b4487dde0460989328c5e13af3e
Binkeurss <nvbinh271104@gmail.com>
2023-11-28 17:34:27 +0700

    Bink

--- TRANSMARIZE/Views/MainWindow.axaml.cs
+++ TRANSMARIZE/Views/MainWindow.axaml.cs
@@ -1,11 +1,30 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Avalonia;
 
 namespace TRANSMARIZE.Views;
 
 public partial class MainWindow : Window
 {
-    public MainWindow()
+    public Button toggleButton;
+    public bool isRunning;
+    public  MainWindow()
     {
         InitializeComponent();
+        toggleButton = this.Find<Button>("ToggleButton");
+        toggleButton.Click += ToggleButtonClick;
+    }
+
+    private void ToggleButtonClick(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        isRunning = !isRunning;
+        UpdateButtonLabel();
+    }
+
+    private void UpdateButtonLabel()
+    {
+        toggleButton.Content = isRunning ? "STOP" : "START";
     }
 }
