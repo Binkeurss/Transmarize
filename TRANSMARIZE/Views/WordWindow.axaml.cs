c37fc1581980b07d441b9783b56f220e77de626c
Binkeurss <nvbinh271104@gmail.com>
2023-12-05 15:22:17 +0700

    Design WordWindow

--- TRANSMARIZE/Views/WordWindow.axaml.cs
+++ TRANSMARIZE/Views/WordWindow.axaml.cs
@@ -8,5 +8,20 @@ namespace TRANSMARIZE.Views
         {
             InitializeComponent();
         }
+
+        public void ExitButton_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            //Environment.Exit(0);
+            this.Close();
+        }
+        private void Window_Deactivated(object? sender, System.EventArgs e)
+        {
+            this.Close();
+        }
+
+        public void HideButton_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
     }
 }
