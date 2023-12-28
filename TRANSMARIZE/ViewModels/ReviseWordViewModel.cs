4da08521cb789f02b2c11ebb6020a4f6b8d9b517
Ngyyen <ngyyenle47@gmail.com>
2023-12-28 16:12:12 +0700

    add Image feature

--- TRANSMARIZE/ViewModels/ReviseWordViewModel.cs
+++ TRANSMARIZE/ViewModels/ReviseWordViewModel.cs
@@ -2,6 +2,12 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using System;
 using System.Collections.Generic;
 using TRANSMARIZE.Model;
+using PexelsDotNetSDK;
+using PexelsDotNetSDK.Api;
+using System.Linq;
+using TRANSMARIZE.Services;
+using System.Threading.Tasks;
+using Avalonia.Media.Imaging;
 
 namespace TRANSMARIZE.ViewModels
 {
@@ -13,12 +19,39 @@ namespace TRANSMARIZE.ViewModels
 		private string learnPhonetic = "nan";
 		[ObservableProperty]
 		private string date = string.Empty;
-		public ReviseWordViewModel() { }
+
+		private string urlImage = string.Empty;
+		[ObservableProperty]
+		private Bitmap imageFromWebsite;
+
+        public ReviseWordViewModel() { }
 		public ReviseWordViewModel(SavedWord learnWord)
 		{
 			LearnContent = learnWord.Content;
 			LearnPhonetic = learnWord.Phonetic;
 			Date = learnWord.Date.ToShortDateString();
+			SearchImage();
 		}
-	}
+
+        public async void SearchImage()
+        {
+            var pexelsClient = new PexelsClient("o2f83GqfSjOliNYQKk2qn23C6QEcDiHIGaKmJkQWtogxMK3zwaMunxfk");
+            var result = await pexelsClient.SearchPhotosAsync(LearnContent);
+			List<PexelsDotNetSDK.Models.Photo> photos = result.photos.ToList();
+			if (photos.Count == 0)
+			{
+				ImageFromWebsite = ImageHelper.LoadFromResource(new Uri("avares://TRANSMARIZE/Assets/just-kidding.png"));
+                return;
+			}
+			foreach (var photo in photos)
+			{
+				if (photo.source.landscape != null)
+				{
+					urlImage = photo.source.landscape;
+					break;
+				}
+			}
+			ImageFromWebsite = await ImageHelper.LoadFromWeb(new Uri(urlImage));
+        }
+    }
 }
